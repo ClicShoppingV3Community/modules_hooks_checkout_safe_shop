bd2e9687019e37251192732b6e8a3a143aa85dde
ClicShopping - e-commerce Solution B2B B2C <39338579+ClicShopping@users.noreply.github.com>
2019-02-18 13:17:07 -0500

    Make the process if only the status is on true

--- includes/Module/Hooks/Shop/CheckoutProcess/CheckoutProcessEsafe.php
+++ includes/Module/Hooks/Shop/CheckoutProcess/CheckoutProcessEsafe.php
@@ -58,67 +58,69 @@
       $CLICSHOPPING_Language = Registry::get('Language');
 
       if (isset($_GET['Checkout']) && isset($_GET['Process'])) {
-        if ( defined('MODULE_HEADER_TAGS_ESAFE_API_SECRETKEY') && defined('MODULE_HEADER_TAGS_ESAFE_SITE_API_CLIENTID') && !empty(MODULE_HEADER_TAGS_ESAFE_API_SECRETKEY) && !empty(MODULE_HEADER_TAGS_ESAFE_SITE_API_CLIENTID)) {
-          $language_code = $CLICSHOPPING_Language->getCode();
+        if (defined('MODULE_HEADER_TAGS_ESAFE_STATUS') && MODULE_HEADER_TAGS_ESAFE_STATUS == 'True') {
+          if ( defined('MODULE_HEADER_TAGS_ESAFE_API_SECRETKEY') && defined('MODULE_HEADER_TAGS_ESAFE_SITE_API_CLIENTID') && !empty(MODULE_HEADER_TAGS_ESAFE_API_SECRETKEY) && !empty(MODULE_HEADER_TAGS_ESAFE_SITE_API_CLIENTID)) {
+            $language_code = $CLICSHOPPING_Language->getCode();
 
-          $info_last_order = static::getOrderInfo();
+            $info_last_order = static::getOrderInfo();
 
-          if (!is_null($info_last_order['orders_id'])) {
-            $name= explode(' ', $info_last_order['customers_name']);
+            if (!is_null($info_last_order['orders_id'])) {
+              $name= explode(' ', $info_last_order['customers_name']);
 
-            $postdata = [
-                          'client_secret'=> MODULE_HEADER_TAGS_ESAFE_API_SECRETKEY,
-                          'client_id'=> MODULE_HEADER_TAGS_ESAFE_SITE_API_CLIENTID,
-                          'grant_type'=>'client_credentials',
-                          'scope'=>'invites_readwrite',
-                        ];
-            $cl = curl_init('https://api.safe.shop/v1/oauth/token');
+              $postdata = [
+                            'client_secret'=> MODULE_HEADER_TAGS_ESAFE_API_SECRETKEY,
+                            'client_id'=> MODULE_HEADER_TAGS_ESAFE_SITE_API_CLIENTID,
+                            'grant_type'=>'client_credentials',
+                            'scope'=>'invites_readwrite',
+                          ];
+              $cl = curl_init('https://api.safe.shop/v1/oauth/token');
 
-            curl_setopt_array($cl, [
-                                      CURLOPT_POST           => true,
-                                      CURLOPT_POSTFIELDS     => $postdata,
-                                      CURLOPT_RETURNTRANSFER => true
-                                    ]
-                              );
+              curl_setopt_array($cl, [
+                                        CURLOPT_POST           => true,
+                                        CURLOPT_POSTFIELDS     => $postdata,
+                                        CURLOPT_RETURNTRANSFER => true
+                                      ]
+                                );
 
-            $content = curl_exec($cl);
-            curl_close($cl);
+              $content = curl_exec($cl);
+              curl_close($cl);
 
-            $json   = json_decode($content);
-            $atoken = $json->access_token;
+              $json   = json_decode($content);
+              $atoken = $json->access_token;
 
-            $data = [
-                      'send_datetime' => $info_last_order['date_purchased'],
-                      'email' => $info_last_order['customers_email_address'],
-                      'firstname' => $name[0],
-                      'lastname' => $name[1],
-                      'telephone' => $info_last_order['customers_telephone'],
-                      'gender' => '',
-                      'order_id' => $info_last_order['orders_id'],
-                      'language' => $language_code,
-                      'city' => $info_last_order['customers_city'],
-                      'country' => $info_last_order['customers_country'],
-                      'segment' => '',
-                      'channel' => 'ClicShopping',
-                      'customer_id' => $info_last_order['customers_id']
-                    ];
+              $data = [
+                        'send_datetime' => $info_last_order['date_purchased'],
+                        'email' => $info_last_order['customers_email_address'],
+                        'firstname' => $name[0],
+                        'lastname' => $name[1],
+                        'telephone' => $info_last_order['customers_telephone'],
+                        'gender' => '',
+                        'order_id' => $info_last_order['orders_id'],
+                        'language' => $language_code,
+                        'city' => $info_last_order['customers_city'],
+                        'country' => $info_last_order['customers_country'],
+                        'segment' => '',
+                        'channel' => 'ClicShopping',
+                        'customer_id' => $info_last_order['customers_id']
+                      ];
 
-            $data_string = json_encode($data);
+              $data_string = json_encode($data);
 
-            $headers = ['Content-Type: application/json',
-                         sprintf('Authorization: Bearer %s', $atoken)
-                       ];
+              $headers = ['Content-Type: application/json',
+                           sprintf('Authorization: Bearer %s', $atoken)
+                         ];
 
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $this->url);
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+              $ch = curl_init();
+              curl_setopt($ch, CURLOPT_URL, $this->url);
+              curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+              curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+              curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+              curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-            $result = curl_exec($ch);
+              $result = curl_exec($ch);
 
-            echo $result;
+              echo $result;
+            }
           }
         }
       }
