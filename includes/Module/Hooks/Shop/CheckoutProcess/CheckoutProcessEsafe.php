384096c543ca4c2c4518a41bef1f4ac5abed01af
ClicShopping - e-commerce Solution B2B B2C <39338579+ClicShopping@users.noreply.github.com>
2019-05-23 19:38:48 -0400

    reformat

--- includes/Module/Hooks/Shop/CheckoutProcess/CheckoutProcessEsafe.php
+++ includes/Module/Hooks/Shop/CheckoutProcess/CheckoutProcessEsafe.php
@@ -1,34 +1,37 @@
 <?php
-/**
- *
- *  @copyright 2008 - https://www.clicshopping.org
- *  @Brand : ClicShopping(Tm) at Inpi all right Reserved
- *  @Licence GPL 2 & MIT
- *  @licence MIT - Portion of osCommerce 2.4
- *  @Info : https://www.clicshopping.org/forum/trademark/
- *
- */
+  /**
+   *
+   * @copyright 2008 - https://www.clicshopping.org
+   * @Brand : ClicShopping(Tm) at Inpi all right Reserved
+   * @Licence GPL 2 & MIT
+   * @licence MIT - Portion of osCommerce 2.4
+   * @Info : https://www.clicshopping.org/forum/trademark/
+   *
+   */
 
   namespace ClicShopping\OM\Module\Hooks\Shop\CheckoutProcess;
 
   use ClicShopping\OM\Registry;
   use ClicShopping\OM\CLICSHOPPING;
 
-  class CheckoutProcessEsafe {
+  class CheckoutProcessEsafe
+  {
 
     protected $url;
 
-    public function __construct() {
+    public function __construct()
+    {
       $CLICSHOPPING_Customer = Registry::get('Customer');
 
       if (!$CLICSHOPPING_Customer->isLoggedOn()) {
         CLICSHOPPING::redirect(null, 'Account&LogIn');
       }
 
-      $this->url ='https://api.safe.shop/v1/invites';
+      $this->url = 'https://api.safe.shop/v1/invites';
     }
 
-    private static function getOrderInfo() {
+    private static function getOrderInfo()
+    {
       $CLICSHOPPING_Db = Registry::get('Db');
       $CLICSHOPPING_Customer = Registry::get('Customer');
 
@@ -54,61 +57,62 @@
       return $QLastOrder->fetch();
     }
 
-    public function execute() {
+    public function execute()
+    {
       $CLICSHOPPING_Language = Registry::get('Language');
 
       if (isset($_GET['Checkout']) && isset($_GET['Process'])) {
         if (defined('MODULE_HEADER_TAGS_ESAFE_STATUS') && MODULE_HEADER_TAGS_ESAFE_STATUS == 'True') {
-          if ( defined('MODULE_HEADER_TAGS_ESAFE_API_SECRETKEY') && defined('MODULE_HEADER_TAGS_ESAFE_SITE_API_CLIENTID') && !empty(MODULE_HEADER_TAGS_ESAFE_API_SECRETKEY) && !empty(MODULE_HEADER_TAGS_ESAFE_SITE_API_CLIENTID)) {
+          if (defined('MODULE_HEADER_TAGS_ESAFE_API_SECRETKEY') && defined('MODULE_HEADER_TAGS_ESAFE_SITE_API_CLIENTID') && !empty(MODULE_HEADER_TAGS_ESAFE_API_SECRETKEY) && !empty(MODULE_HEADER_TAGS_ESAFE_SITE_API_CLIENTID)) {
             $language_code = $CLICSHOPPING_Language->getCode();
 
             $info_last_order = static::getOrderInfo();
 
             if (!is_null($info_last_order['orders_id'])) {
-              $name= explode(' ', $info_last_order['customers_name']);
+              $name = explode(' ', $info_last_order['customers_name']);
 
               $postdata = [
-                            'client_secret'=> MODULE_HEADER_TAGS_ESAFE_API_SECRETKEY,
-                            'client_id'=> MODULE_HEADER_TAGS_ESAFE_SITE_API_CLIENTID,
-                            'grant_type'=>'client_credentials',
-                            'scope'=>'invites_readwrite',
-                          ];
+                'client_secret' => MODULE_HEADER_TAGS_ESAFE_API_SECRETKEY,
+                'client_id' => MODULE_HEADER_TAGS_ESAFE_SITE_API_CLIENTID,
+                'grant_type' => 'client_credentials',
+                'scope' => 'invites_readwrite',
+              ];
               $cl = curl_init('https://api.safe.shop/v1/oauth/token');
 
               curl_setopt_array($cl, [
-                                        CURLOPT_POST           => true,
-                                        CURLOPT_POSTFIELDS     => $postdata,
-                                        CURLOPT_RETURNTRANSFER => true
-                                      ]
-                                );
+                  CURLOPT_POST => true,
+                  CURLOPT_POSTFIELDS => $postdata,
+                  CURLOPT_RETURNTRANSFER => true
+                ]
+              );
 
               $content = curl_exec($cl);
               curl_close($cl);
 
-              $json   = json_decode($content);
+              $json = json_decode($content);
               $atoken = $json->access_token;
 
               $data = [
-                        'send_datetime' => $info_last_order['date_purchased'],
-                        'email' => $info_last_order['customers_email_address'],
-                        'firstname' => $name[0],
-                        'lastname' => $name[1],
-                        'telephone' => $info_last_order['customers_telephone'],
-                        'gender' => '',
-                        'order_id' => $info_last_order['orders_id'],
-                        'language' => $language_code,
-                        'city' => $info_last_order['customers_city'],
-                        'country' => $info_last_order['customers_country'],
-                        'segment' => '',
-                        'channel' => 'ClicShopping',
-                        'customer_id' => $info_last_order['customers_id']
-                      ];
+                'send_datetime' => $info_last_order['date_purchased'],
+                'email' => $info_last_order['customers_email_address'],
+                'firstname' => $name[0],
+                'lastname' => $name[1],
+                'telephone' => $info_last_order['customers_telephone'],
+                'gender' => '',
+                'order_id' => $info_last_order['orders_id'],
+                'language' => $language_code,
+                'city' => $info_last_order['customers_city'],
+                'country' => $info_last_order['customers_country'],
+                'segment' => '',
+                'channel' => 'ClicShopping',
+                'customer_id' => $info_last_order['customers_id']
+              ];
 
               $data_string = json_encode($data);
 
               $headers = ['Content-Type: application/json',
-                           sprintf('Authorization: Bearer %s', $atoken)
-                         ];
+                sprintf('Authorization: Bearer %s', $atoken)
+              ];
 
               $ch = curl_init();
               curl_setopt($ch, CURLOPT_URL, $this->url);
